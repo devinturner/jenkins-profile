12d85c5e4635c17f61c8c81b159c1a582db168e3
Alexander Braverman <abraverm@redhat.com>
2018-06-18 13:27:06 +0300

    initial CI

--- controls/example.rb
+++ controls/example.rb
@@ -1,6 +1,3 @@
-# encoding: utf-8
-# copyright: 2018, The Authors
-
 title 'sample section'
 
 # you can also use plain tests
@@ -9,11 +6,11 @@ describe file('/tmp') do
 end
 
 # you add controls here
-control 'tmp-1.0' do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title 'Create /tmp directory'             # A human-readable title
+control 'tmp-1.0' do                # A unique ID for this control
+  impact 0.7                        # The criticality, if this control fails.
+  title 'Create /tmp directory'     # A human-readable title
   desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
+  describe file('/tmp') do          # The actual test
     it { should be_directory }
   end
 end
